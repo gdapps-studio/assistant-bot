33c2f69122df3ad5ab83835a639175aefd592a3e
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2023-07-05 03:53:25 +0500

    Update webhook.mjs

--- api/webhook.mjs
+++ api/webhook.mjs
@@ -4,5 +4,6 @@ import {bot, secretToken} from "../src/bot.mjs";
 // Handler to set webhook url based on request headers
 export default setWebhookCallback(bot, {
     secret_token: secretToken,
+    path: "api/update",
     onError: "return"
 });
