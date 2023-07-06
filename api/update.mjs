53da8a3a30053bce6bc52f5672a969a59d6f0d73
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-07-06 17:33:15 +0500

    Updated template

--- api/update.mjs
+++ api/update.mjs
@@ -2,4 +2,7 @@ import {webhookCallback} from "grammy";
 import {bot, secretToken} from "../src/bot.mjs";
 
 // Default grammY handler for incoming updates via webhooks
-export default webhookCallback(bot, "http", {secretToken});
+export default webhookCallback(bot, "http", {
+    timeoutMilliseconds: 9_000,
+    secretToken
+});
