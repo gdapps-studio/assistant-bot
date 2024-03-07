1e95e3235f83ce7c753d5fcaa94635fbc0958104
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-03-07 20:41:17 +0500

    Revert default exports for api functions

--- api/update.mjs
+++ api/update.mjs
@@ -2,7 +2,7 @@ import {bot, secretToken} from "../src/bot.mjs";
 import {webhookCallback} from "grammy";
 
 // Default grammY handler for incoming updates via webhooks
-export const POST = webhookCallback(bot, "std/http", {
+export default webhookCallback(bot, "http", {
     timeoutMilliseconds: 9_000,
     secretToken
 });
--- api/webhook.mjs
+++ api/webhook.mjs
@@ -2,7 +2,7 @@ import {bot, secretToken} from "../src/bot.mjs";
 import {setWebhookCallback} from "vercel-grammy";
 
 // Handler to set webhook url based on request headers
-export const POST = setWebhookCallback(bot, {
+export default setWebhookCallback(bot, {
     path: "api/update",
     onError: "return",
     secretToken
